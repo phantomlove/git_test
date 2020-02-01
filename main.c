9233b5693d4b885fff7b85f899395f518d3ce44f
wukong <271090963@qq.com>
2020-02-01 13:05:29 +0800

    add main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2020年02月01日 13时04分19秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dr. Fritz Mehner (mn), mehner@fh-swf.de
+ *        Company:  FH Südwestfalen, Iserlohn
+ *
+ * =====================================================================================
+ */
+int main()
+{
+	printf("hello git!\n");
+	return 0;
+}
+
